079ef9952ddb94e1d773a4bedcb868b64f0b5380
momansethi <104350782+momansethi@users.noreply.github.com>
2022-04-25 19:14:52 +0530

    Add files via upload

---
+++ averageof3no.py
@@ -0,0 +1,5 @@
+a=float(input("first number="))
+b=float(input("second number="))
+c=float(input("third number="))
+d=(a+b+c)/3
+print("Average=",d)
---
+++ sorted marks.py
@@ -0,0 +1,7 @@
+a=int(input("Marks of student 1 :"))
+b=int(input("Marks of student 2:"))
+c=int(input("Marks of student 3:"))
+d=int(input("Marks of student 4:"))
+e=int(input("Marks of student 5:"))
+sortedmarks=[a,b,c,d,e]
+print("sortedmarks",sortedmarks)
